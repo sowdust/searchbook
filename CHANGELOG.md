cf7ed915a8a44084b67acd73a85fd4de7d2c2797
Mattia <sowdust@gmail.com>
2019-07-09 10:20:49 +0200

    Auto update notification system

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Version 0.3 (2019-07-09)
+
+* Update notification system
+
+# Version 0.2 (2019-06-17)
+
+* Added feature to search for photos and videos elements
--- popup/popup.js
+++ popup/popup.js
@@ -8,6 +8,7 @@
   * 
 */
 
+
 function set_query() {
   query = document.getElementById('query').value;
   browser.runtime.sendMessage({
@@ -54,10 +55,50 @@ function email_link() {
   div.innerHTML = atob("PGEgaHJlZj0ibWFpbHRvOm1hdHRpYS52aW5jaUBtZWRpYXNlcnZpY2UubmV0Ij5zb3dkdXN0PC9hPg==");
 }
 
+function show_updates(new_version) {
+  var update_div = document.getElementById('updates');
+  update_div.style.display = 'block';
+  var new_version_div = document.getElementById('new-version');
+  new_version_div.innerHTML = new_version;
+}
+
+function check_updates() {
+  var last_update_check = window.localStorage.getItem('searchbook-last-update-check');
+  var curr_time = Date.now();
+  var manifest = browser.runtime.getManifest();
+  var current_version = manifest.version;
+  if(!last_update_check || (curr_time - last_update_check) > (12*60*60*1000)) {    
+    console.log('Checking updates...');
+    var update_url = 'https://raw.githubusercontent.com/sowdust/searchbook/master/manifest.json';
+    var http = new XMLHttpRequest();
+    http.open("GET", update_url);
+    http.send();
+    var new_manifest = '';
+    http.onreadystatechange = (e) => {
+      if(http.readyState === 4 && http.status === 200) {
+        new_manifest = JSON.parse(http.responseText);
+        if(new_manifest.version > current_version) {
+          show_updates(new_manifest.version);
+          window.localStorage.setItem('searchbook-latest-version',new_manifest.version);
+        }
+        window.localStorage.setItem('searchbook-last-update-check',curr_time);
+      }
+    }  
+  }else{
+    var new_vers_av = window.localStorage.getItem('searchbook-latest-version');
+    if( new_vers_av > current_version) {
+      show_updates(new_vers_av)
+    }
+    return;
+  }
+}
+
 // fill Entity ID and current query
 document.onload = fill_values();
 // add email address 
 document.onload = email_link();
+// check for updates
+document.onload = check_updates();
 
 
 document.addEventListener("click", (e) => {
